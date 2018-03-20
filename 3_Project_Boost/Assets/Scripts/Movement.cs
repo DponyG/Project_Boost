4df1fb762e41bc44979baa43e1aba118b22de1b4
Darkpony <dponyg@gmail.com>
2018-03-19 17:58:31 -0800

    Pushing repo of Project Boost

---
+++ 3_Project_Boost/Assets/Scripts/Movement.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+
+public class Movement : MonoBehaviour {
+
+    [SerializeField] float rcsThrust = 100.0f;
+    [SerializeField] float mainThrust = 100.0f;
+    [SerializeField] float levelLoadDelay = 2f;
+
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip death;
+    [SerializeField] AudioClip finish;
+
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem deathParticles;
+    [SerializeField] ParticleSystem finishParticles;
+
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+
+
+    enum State { Alive, Dying, Trancending};
+    State state = State.Alive;
+
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+      
+        if (state == State.Alive) {
+            RespondToThrustInput();
+            RespondToRotateInput();
+        }
+	}
+
+
+    void OnCollisionEnter(Collision collision) {
+        if(state != State.Alive) { return; }
+        switch (collision.gameObject.tag) {
+            case "Friendly":
+                break;
+
+            case "Finish":
+                StartFinish();
+                break;
+            default:
+                StartDeath();
+                break;
+        }
+    }
+
+    private void StartFinish() {
+        state = State.Trancending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(finish);
+        finishParticles.Play();
+        Invoke("LoadNextLevel", levelLoadDelay); //parameterise time
+    }
+
+    private void StartDeath() {
+        state = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(death);
+        deathParticles.Play();
+        Invoke("LoadFirstLevel", levelLoadDelay);
+    }
+
+    private void LoadNextLevel() {
+        SceneManager.LoadScene(1); 
+    }
+
+    private void LoadFirstLevel() {
+        SceneManager.LoadScene(0); 
+    }
+
+    private void RespondToThrustInput() {
+
+        if (Input.GetKey(KeyCode.Space)) {
+
+            ApplyThrust();
+           
+        } else {
+            audioSource.Stop();
+          //  mainEngineParticles.Stop(); // TODO NOT WORKING FIX 
+        }
+    }
+
+    private void ApplyThrust() {
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust* Time.deltaTime );
+        if (!audioSource.isPlaying) {
+            audioSource.PlayOneShot(mainEngine);
+        }
+
+      //  mainEngineParticles.Play();
+
+
+    }
+
+    private void RespondToRotateInput() {
+
+        rigidBody.freezeRotation = true; //take manual control of rotation
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+    
+   
+        if (Input.GetKey(KeyCode.A)) {
+
+            transform.Rotate(Vector3.forward* rotationThisFrame);
+        } 
+        else if (Input.GetKey(KeyCode.D)) {
+   
+            transform.Rotate(-Vector3.forward* rotationThisFrame);
+        }
+
+        rigidBody.freezeRotation = false; //resume rotation
+    }
+
+}
---
+++ 3_Project_Boost/Assets/Scripts/Oscillator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour {
+
+    [SerializeField] Vector3 movementVector = new Vector3(10, 0, 0);
+    [SerializeField] float period = 2f;
+   
+
+    private Vector3 startingPos;
+
+    //todo remove from inspector later
+
+    float movementFactor; //0 for not moved, 1 for fully moved.
+
+	// Use this for initialization
+	void Start () {
+        startingPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //set movementFactor automatically
+        if(period <= Mathf.Epsilon) { return ; }
+        float cycles = Time.time / period; // grows continually from 0
+
+        const float tau = Mathf.PI * 2; // 2pi 6.28
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        movementFactor = rawSinWave / 2f + .05f;
+
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPos + offset;
+	}
+}
